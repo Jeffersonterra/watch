160f8486697348d26f9a31a2717c3f3fb3d54e05
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-09 10:03:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+window.onload = function(){
+    var seconds = 00;
+    var tens = 00;
+    var OutputSeconds = document.getElementById("second");
+    var OutputTens = document.getElementById("tens");
+    var buttonStart = document.getElementById("btn-start");
+    var buttonStop = document.getElementById("btn-stop");
+    var buttonReset = document.getElementById("btn-reset");
+    var Interval;
+
+    buttonStart.addEventListener('click', () => {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);  // millisecond 10 = 0.01 second
+    });
+
+
+
+    function startTimer(){
+        tens++;
+        if(tens <= 9){
+            OutputTens.innerHTML = "0" + tens;
+        }
+
+        if(tens > 9){
+            OutputTens.innerHTML = tens;
+        }
+
+        if(tens > 99){
+            seconds++;
+            OutputSeconds.innerHTML = "0" + seconds;
+            tens = 0;
+            OutputTens.innerHTML = "0" + 0;
+        }
+
+        if(seconds > 9){
+            OutputSeconds.innerHTML = seconds;
+        }
+    }
+}
